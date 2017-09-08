ecd4ec3eee3eabd3fe5aedd89cd6597ad9340348
Peng Jiang <pengj@usc.edu>
2017-09-07 23:24:04 -0700

    first commit

---
+++ gold.py
@@ -0,0 +1,41 @@
+from flask import Flask,redirect,render_template,request,session,jsonify
+import random
+import time
+app = Flask(__name__)
+app.secret_key = 'NinjaGold'
+@app.route('/')
+def index():
+	if not 'gold' in session:
+		session['gold']=0
+		session['activity']=[]
+	return render_template('gold.html')
+@app.route('/process_money',methods=['post'])
+def submit():
+	case = request.form['building']
+	if case == 'farm':
+		session['earned'] = random.randint(10,20)
+		session['gold']+=session['earned']
+		session['activity'].append("Earned {} golds from the farm!({})".format(session['earned'],time.strftime('%y/%m/%d %I:%M %p')))
+	elif case == 'cave':
+		session['earned'] = random.randint(5,10)
+		session['gold']+=session['earned']
+		session['activity'].append("Earned {} golds from the cave!({})".format(session['earned'],time.strftime('%y/%m/%d %I:%M %p')))
+	elif case == 'house':
+		session['earned'] = random.randint(2,5)
+		session['gold']+=session['earned']
+		session['activity'].append("Earned {} golds from the house!({})".format(session['earned'],time.strftime('%y/%m/%d %I:%M %p')))
+	elif case == 'casino':
+		session['earned']=random.randint(-50,50)
+		print session['earned']
+		session['gold']+=session['earned']
+		if session['earned'] >= 0:
+			session['activity'].append("Entered a casino and lost {} golds... Ouch..({})".format(session['earned'],time.strftime('%y/%m/%d %I:%M %p')))
+		else:
+			session['activity'].append("Earned {} golds from the casino!({})".format(0-session['earned'],time.strftime('%y/%m/%d %I:%M %p')))
+	# return False
+	return redirect('/')
+# @app.route('/response')
+# def res():
+# 	response = {'gold':session['gold'],'activity': session['activity'],'earned':session['earned']}
+# 	return jsonify(response)
+app.run(debug=True)
---
+++ static/js/gold.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	// $('form').submit(function(){
+	// 	$.get('/response',function(res){
+	// 		var gold = res.gold
+	// 		if (res.activity.length>0){
+	// 			var active = res.activity[res.activity.length-1]
+	// 			console.log(active)
+	// 		}
+			
+	// 		var earned = res.earned
+	// 		console.log(gold)
+			
+	// 		console.log(earned)
+
+	// 	},'json')
+	// 	return false
+	// })
+})
